b22d63261aa07d211390107ee091594e8b88afb7
e-lubrini <lubrinie@gmail.com>
2022-05-16 10:55:37 +0200

    set up gobid tool

---
+++ img2txt/tools/conversion/grobid/grobid.py
@@ -0,0 +1,25 @@
+import json
+import os
+import subprocess
+
+import requests
+
+import subprocess
+
+
+conv_config_path = os.path.join('tools','conversion','grobid','grobid_config.json')
+with open(conv_config_path, 'r') as config_file:
+    config = json.load(config_file)
+
+def extract_emb_txt(pdf_filepath,
+                    grobid_inst_path=config['grobid_inst_path'],
+                    config_path=config['config_path'],
+                    GROBID_URL=config['GROBID_URL'],
+                    url=config['url']
+                    ):
+
+    subprocess.run(['bash','../grobid/gradlew run'])
+
+    xml = requests.post(url, files={'input': open(pdf_filepath, 'rb')})
+
+    return xml.text
